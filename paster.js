c7638e214e85f76a9636aa770ef6d298e91906a8
Karol Swieca <swieca.karol@gmail.com>
2018-04-07 21:12:48 +0200

    basic insertion

--- paster.js
+++ paster.js
@@ -1,32 +1,65 @@
 const pressed = []
 const MAX_LENGTH = 10
+const test_code = '/te'
+const test_insert = 'super testowe lelel'
 
-document.addEventListener('keydown', function (event) {
-  const key = event.key; // "a", "1", "Shift", etc.
-  console.log('keydown')
-  console.log(event)
-  console.log(key)
-});
+const snippets = [
+  {
+    code: '/te',
+    text: 'super testowe lele'
+  },
+  {
+    code: '/te',
+    text: 'super testowe lele'
+  },
+]
+
+function textAreaHandle(textArea, foundCode, snippetInsert){
+  console.log('podmienianko ')
+  console.log(textArea.value)
+  console.log(textArea.value.includes(test_code))
+  textArea.value = textArea.value.replace(foundCode, snippetInsert)
+
+}
+
 document.addEventListener('keyup', function (event) {
   const key = event.key; // "a", "1", "Shift", etc.
-  console.log('keyup')
+  pressed.push(key)
+
+  pressed.splice(-MAX_LENGTH - 1, pressed.length - MAX_LENGTH)
+  console.log(`czy zawiera? ${pressed.join('').includes(test_code)}`)
+  console.log(`current target ${event.target.type}`)
   console.log(event)
-  console.log(key)
+  console.log(pressed)
+
+  if (pressed.join('').includes(test_code) && event.target.type == 'textarea') {
+
+    textAreaHandle(event.target, test_code, test_insert)
+    pressed.length = 0 //clear array
+  }
 });
 
-document.onclick = function (e) {
-  // e.target, e.srcElement and e.toElement contains the element clicked.
-  console.log("User clicked a 2 " + e.target.nodeName + " element.");
-};
-
 // document.addEventListener('keydown', function (event) {
-//     const key = event.key; // "a", "1", "Shift", etc.
-//     pressed.push(key)
-//     pressed.splice(-MAX_LENGTH - 1, pressed.length - MAX_LENGTH)
-//     console.log(pressed)
-//     console.log(event)
-//     console.log(key)
+//   const key = event.key; // "a", "1", "Shift", etc.
+//   console.log('keydown')
+//   console.log(event)
+//   console.log(key)
 // });
+// document.addEventListener('keyup', function (event) {
+//   const key = event.key; // "a", "1", "Shift", etc.
+//   console.log('keyup')
+//   console.log(event)
+//   console.log(key)
+// });
+
+// document.onclick = function (e) {
+//   // e.target, e.srcElement and e.toElement contains the element clicked.
+//   console.log("User clicked a 2 " + e.target.nodeName + " element.");
+// };
+
+
+
+
 
 // document.onclick = function (e) {
 //     // e.target, e.srcElement and e.toElement contains the element clicked.
@@ -60,21 +93,3 @@ document.onclick = function (e) {
 //     // Change this to div.childNodes to support multiple top-level nodes
 //     return div.firstChild;
 // }
-
-
-// observerConfig = {
-//   attributes: !0,
-//   childList: !0,
-//   subtree: !0,
-//   attributeFilter: ["contenteditable"]
-// }
-
-// let callback = function (mutationsList) {
-//   for (var mutation of mutationsList) {
-//     console.log('The ' + mutation.attributeName);
-//   }
-// };
-
-// let observer = new MutationObserver(callback);
-
-// observer.observe(document, observerConfig);
